f7fba246bc5f7165f3c692b5ac257b2411312d39
heroleo001 <leo.heriban@gmail.com>
2022-01-15 07:31:37 +0100

    Initial commit

---
+++ src/main/java/net/leo/herotech/HeroTech.java
@@ -0,0 +1,16 @@
+package net.leo.herotech;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HeroTech implements ModInitializer {
+
+	public static final String MDO_ID = "herotech";
+	public static final Logger LOGGER = LogManager.getLogger(MDO_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
